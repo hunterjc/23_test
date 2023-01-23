8d55867e6c4f630e09801d6a654582fa0bce765e
Chinmoy Hembram <53371290+hunterjc@users.noreply.github.com>
2023-01-23 15:48:17 +0530

    Update jj.js

--- public/jj.js
+++ public/jj.js
@@ -1,5 +1,23 @@
-import data from "./data";
-function update() {
-  const textContent = document.querySelectorAll(".card-footer");
-  console.log(textContent.length);
-}
+import data from "./data.js";
+
+const contentContainer = document.querySelector(".content");
+
+const renderContent = () => {
+  let innerHtml = "";
+  data.forEach((obj) => {
+    innerHtml += `
+    <div class="col-md-4">
+    <div class="card h-100">
+        <div class="card-body">
+            <h2 class="card-title">${obj.Name}</h2>
+            <p class="card-footer">${obj.ShortDesc}</p>
+        </div>
+    </div>
+    </div>    
+    
+    `;
+  });
+  contentContainer.innerHTML = innerHtml;
+};
+
+renderContent();
